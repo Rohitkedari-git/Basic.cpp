034021ca8e4b3654e97b72fead99f4c000651d9f
Rohitkedari-git <rohitkedari017@gmail.com>
2026-07-31 23:03:14 +0530

    Add basic input/output functionality in C++

---
+++ BasicInputOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int userNumber;
+
+    cout << "Enter an integer: ";
+    cin >> userNumber;
+
+    cout << "You entered: " << userNumber << endl;
+
+    return 0;
+}
+
